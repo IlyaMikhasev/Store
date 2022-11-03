5c635c54f9986be2004be01bff2e35ac8efef4c2
илья михасев <psfxslon@rambler.ru>
2022-11-03 22:29:35 +0500

    встраиваемую функцию надо поностью вписывать в заголовочный файл

--- Store/Products.cpp
+++ Store/Products.cpp
@@ -7,9 +7,7 @@ void print_prod(product &P){
 	std::cout << "Цена =              " << P.price << "\n\n";
 }
 
-int full_price(product &P){
-	return P.number*P.price;
-}
+
 
 void sell(product &P, int num){
 	if (num > P.number)
--- Store/Products.h
+++ Store/Products.h
@@ -6,5 +6,7 @@ struct product{
 	int number ;	
 }apple{ "€блоко",20,15 }, orange{ "апельсин", 7, 19 }, pineapple{ "ананас",80 };
 void print_prod(product& P);
-int full_price(product& P);
+inline int full_price(product& P) {
+	return P.number * P.price;
+}
 void sell(product &P,int num);
